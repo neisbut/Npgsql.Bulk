03b7b791ef5977115641f6d75480c2812972141d
amburin <nikolay.op@gmail.com>
2019-05-10 16:25:39 +0300

    ef_core_migration added migrations

---
+++ src/Npgsql.Bulk.SampleRunner.DotNetStandard20/Migrations/20190510131433_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using NpgsqlTypes;
+
+namespace Npgsql.Bulk.SampleRunner.DotNetStandard20.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "addresses",
+                schema: "public",
+                columns: table => new
+                {
+                    address_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    street_name = table.Column<string>(nullable: false),
+                    house_number = table.Column<int>(nullable: false),
+                    extra_house_number = table.Column<int>(nullable: true),
+                    postal_code = table.Column<string>(nullable: false),
+                    range = table.Column<NpgsqlRange<DateTime>>(nullable: false),
+                    created_at = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_addresses", x => x.address_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "addresses",
+                schema: "public");
+        }
+    }
+}
